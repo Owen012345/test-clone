fb137645d287341daab42a9deba7111a038198a4
Owen <jaewoo.song@dtonic.io>
2024-08-27 14:28:51 +0900

    Add: C4_N04, C4_N05 추가

---
+++ src/components/nodes/transformation/C4_N05.vue
@@ -0,0 +1,103 @@
+<template lang="">
+  <v-container fluid>
+    <CustomCard title="Column Select">
+      <CustomSelectList
+        :leftHeader="'Excludes'"
+        :rightHeader="'Includes'"
+        v-model="formData.columnSelectionArray"
+        :items="[]"
+      >
+      </CustomSelectList>
+    </CustomCard>
+    <CustomCard title="Start Time Settings">
+      <v-radio-group v-model="formData.startTimeSettingEnum" hide-details>
+        <template v-for="(item, index) in schema.properties.startTimeSettingEnum.enum" :key="index">
+          <v-radio :label="item" :value="item"></v-radio>
+          <CustomCard v-if="item === 'Custom'">
+            <span>Date</span>
+            <v-text-field
+              :type="schema.properties.startTimeCustomDate.type"
+              :disabled="formData.startTimeSettingEnum !== 'Custom'"
+              hide-details
+              v-model="formData.startTimeCustomDate"
+            ></v-text-field>
+            <span>Time</span>
+            <v-text-field
+              :type="schema.properties.startTimeCustomTime.type"
+              :disabled="formData.startTimeSettingEnum !== 'Custom'"
+              hide-details
+              v-model="formData.startTimeCustomTime"
+            ></v-text-field>
+          </CustomCard>
+        </template>
+      </v-radio-group>
+    </CustomCard>
+    <CustomCard title="End Time Settings">
+      <v-radio-group v-model="formData.endTimeSettingEnum" hide-details>
+        <template v-for="(item, index) in schema.properties.endTimeSettingEnum.enum" :key="index">
+          <v-radio :label="item" :value="item"></v-radio>
+          <CustomCard v-if="item === 'Custom'">
+            <span>Date</span>
+            <v-text-field
+              :type="schema.properties.endTimeCustomDate.type"
+              :disabled="formData.endTimeSettingEnum !== 'Custom'"
+              hide-details
+              v-model="formData.endTimeCustomDate"
+            ></v-text-field>
+            <span>Time</span>
+            <v-text-field
+              :type="schema.properties.endTimeCustomTime.type"
+              :disabled="formData.endTimeSettingEnum !== 'Custom'"
+              hide-details
+              v-model="formData.endTimeCustomTime"
+            ></v-text-field>
+          </CustomCard>
+        </template>
+      </v-radio-group>
+    </CustomCard>
+    <!-- <CustomCard title="Replace/Append Selection">
+      <v-radio-group v-model="formData.replaceAppendSelectionEnum" inline hide-details>
+        <v-radio
+          v-for="(item, index) in schema.properties.replaceAppendSelectionEnum.enum"
+          :key="index"
+          :label="item"
+          :value="item"
+        >
+        </v-radio>
+      </v-radio-group>
+    </CustomCard>
+    <CustomCard title="Type and Format Selection">
+      <span>New Type</span>
+      <v-select
+        :items="schema.properties.newTypeEnum.enum"
+        v-model="formData.newTypeEnum"
+        hide-details
+      ></v-select>
+      <span>Date Format</span>
+      <v-select
+        :items="schema.properties.dateFormatEnum.enum"
+        v-model="formData.dateFormatEnum"
+        hide-details
+      ></v-select>
+      <span>Locale</span>
+      <v-select
+        :items="schema.properties.localeEnum.enum"
+        v-model="formData.localeEnum"
+        hide-details
+      ></v-select>
+    </CustomCard> -->
+  </v-container>
+</template>
+<script>
+import CustomCard from '@/components/custom/CustomCard.vue'
+import formMixin from '@/components/mixins/formMixin'
+import CustomSelectList from '@/components/custom/CustomSelectList.vue'
+export default {
+  name: 'C4_N05',
+  components: {
+    CustomCard,
+    CustomSelectList
+  },
+  mixins: [formMixin]
+}
+</script>
--- src/views/workflow/details/ComponentRender.vue
+++ src/views/workflow/details/ComponentRender.vue
@@ -22,6 +22,8 @@ import C3_N06 from '@/components/nodes/preprocess/C3_N06.vue'
 import C4_N01 from '@/components/nodes/transformation/C4_N01.vue'
 import C4_N02 from '@/components/nodes/transformation/C4_N02.vue'
 import C4_N03 from '@/components/nodes/transformation/C4_N03.vue'
+import C4_N04 from '@/components/nodes/transformation/C4_N04.vue'
+import C4_N05 from '@/components/nodes/transformation/C4_N05.vue'
 
 import C5_N01 from '@/components/nodes/datamining/C5_N01.vue'
 export default {
@@ -44,6 +46,8 @@ export default {
     C4_N01,
     C4_N02,
     C4_N03,
+    C4_N04,
+    C4_N05,
     C5_N01
   },
   props: {
