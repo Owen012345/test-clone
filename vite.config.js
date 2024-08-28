7841359144685a1fc0f572d83e38087ae2dd7d64
Owen <jaewoo.song@dtonic.io>
2024-08-28 17:18:57 +0900

    Update : vite env load 옵션 추가

--- vite.config.js
+++ vite.config.js
@@ -1,32 +1,52 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import vuetify from 'vite-plugin-vuetify'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-    vuetify({
-      styles: {
-        configFile: 'src/styles/vuetify/settings.scss',
-      },
-    })
-  ],
-  optimizeDeps: {
-    exclude: ["vuetify"]
-  },
-  css: { preprocessorOptions: {
-    sass: {
-      api: 'modern'
+export default defineConfig(({ mode }) => {
+  // Load env file based on `mode` in the current working directory.
+  // The third parameter is set to '' to load all env variables regardless of the `VITE_` prefix.
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    plugins: [
+      vue(),
+      vueDevTools(),
+      vuetify({
+        styles: {
+          configFile: 'src/styles/vuetify/settings.scss'
+        }
+      })
+    ],
+    optimizeDeps: {
+      exclude: ['vuetify']
     },
-}, },
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+    css: {
+      preprocessorOptions: {
+        sass: {
+          api: 'modern'
+        }
+      }
+    },
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
+    },
+    server: {
+      proxy: {
+        // '/argo' 경로에 대한 프록시 설정
+        '/argo': {
+          target: env.VITE_ARGO_WORKFLOW_API_URL,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/argo/, '')
+        }
+      }
+    },
+    define: {
+      __APP_ENV__: JSON.stringify(env.APP_ENV)
     }
   }
 })
